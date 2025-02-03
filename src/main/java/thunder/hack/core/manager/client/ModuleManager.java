c5b975533c11196b844f1640c68b397e13564b82
c0re UNIX <iweyrtx@yandex.ru>
2025-02-03 14:17:43 +0000

    W

--- src/main/java/thunder/hack/core/manager/client/ModuleManager.java
+++ src/main/java/thunder/hack/core/manager/client/ModuleManager.java
@@ -260,6 +260,7 @@ public class ModuleManager implements IManager {
     public static FOV fov = new FOV();
     public static ESP esp = new ESP();
     public static RPC rpc = new RPC();
+    public static AutoFarm autoFarm = new AutoFarm();
 
     public ModuleManager() {
         for (Field field : getClass().getDeclaredFields()) {
